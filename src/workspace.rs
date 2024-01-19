57fdb4060409c030a932590f6293b58ed0946392
Lachance, Alexandre <alexandrelachance@me.com>
2024-01-19 16:32:19 -0500

    Split up Workspace methods into FileManagement and Language Actions traits.

--- src/workspace.rs
+++ src/workspace.rs
@@ -9,6 +9,33 @@ use tower_lsp::lsp_types::{
 
 use crate::{file::File, settings::Settings};
 
+pub trait FileManagement {
+    fn get_file(&self, url: &Url) -> Option<&File>;
+    fn get_file_mut(&mut self, url: &Url) -> Option<&mut File>;
+    fn add_file(&mut self, url: Url, content: &str);
+    fn update_file(&mut self, url: &Url, changes: Vec<TextDocumentContentChangeEvent>);
+}
+
+pub trait LanguageActions {
+    fn get_definition_location(&self, url: &Url, symbol_position: Position) -> Option<Location>;
+    fn get_semantic_tokens(&self, url: &Url) -> Option<SemanticTokensResult>;
+    fn rename_symbol(
+        &mut self,
+        url: &Url,
+        symbol_position: Position,
+        new_name: String,
+    ) -> Option<WorkspaceEdit>;
+    fn get_completion(
+        &self,
+        url: &Url,
+        position: Position,
+        context: Option<CompletionContext>,
+    ) -> Option<Vec<CompletionItem>>;
+    fn get_hover_info(&self, url: &Url, position: Position) -> Option<HoverContents>;
+    fn get_quick_diagnostics(&self, url: &Url) -> Vec<Diagnostic>;
+    fn get_full_diagnostics(&self, url: &Url) -> Vec<Diagnostic>;
+}
+
 pub struct Workspace {
     settings: Settings,
     url_node_map: HashMap<Url, NodeIndex>,
@@ -26,40 +53,45 @@ impl Workspace {
         }
     }
 
-    pub fn get_file(&self, url: &Url) -> Option<&File> {
+    pub fn update_settings(&mut self, settings: Value) {
+        self.settings = Settings::parse(settings);
+        info!("Settings: {:?}", self.settings);
+    }
+}
+
+impl FileManagement for Workspace {
+    fn get_file(&self, url: &Url) -> Option<&File> {
         let index = self.url_node_map.get(url)?;
         self.files_graph.node_weight(*index)
     }
 
-    pub fn get_file_mut(&mut self, url: &Url) -> Option<&mut File> {
+    fn get_file_mut(&mut self, url: &Url) -> Option<&mut File> {
         let index = self.url_node_map.get(url)?;
         self.files_graph.node_weight_mut(*index)
     }
 
-    pub fn add_file(&mut self, url: Url, content: &str) {
+    fn add_file(&mut self, url: Url, content: &str) {
         let index =
             self.files_graph
                 .add_node(File::new(url.clone(), content, self.tree_sitter_language));
         self.url_node_map.insert(url, index);
     }
 
-    pub fn update_file(&mut self, url: &Url, changes: Vec<TextDocumentContentChangeEvent>) {
+    fn update_file(&mut self, url: &Url, changes: Vec<TextDocumentContentChangeEvent>) {
         let file = self.get_file_mut(url).unwrap();
 
         file.update(changes);
     }
+}
 
-    pub fn get_definition_location(
-        &self,
-        url: &Url,
-        symbol_position: Position,
-    ) -> Option<Location> {
+impl LanguageActions for Workspace {
+    fn get_definition_location(&self, url: &Url, symbol_position: Position) -> Option<Location> {
         let file = self.get_file(url)?;
 
         file.get_definition_location(symbol_position)
     }
 
-    pub fn rename_symbol(
+    fn rename_symbol(
         &mut self,
         url: &Url,
         symbol_position: Position,
@@ -70,13 +102,13 @@ impl Workspace {
         file.rename_symbol(symbol_position, new_name)
     }
 
-    pub fn get_semantic_tokens(&self, url: &Url) -> Option<SemanticTokensResult> {
+    fn get_semantic_tokens(&self, url: &Url) -> Option<SemanticTokensResult> {
         let file = self.get_file(url)?;
 
         Some(file.get_semantic_tokens())
     }
 
-    pub fn get_completion(
+    fn get_completion(
         &self,
         url: &Url,
         position: Position,
@@ -87,13 +119,13 @@ impl Workspace {
         file.get_completion_list(position, context)
     }
 
-    pub fn get_hover_info(&self, url: &Url, position: Position) -> Option<HoverContents> {
+    fn get_hover_info(&self, url: &Url, position: Position) -> Option<HoverContents> {
         let file = self.get_file(url)?;
 
         file.get_hover_info(position)
     }
 
-    pub fn get_quick_diagnostics(&self, url: &Url) -> Vec<Diagnostic> {
+    fn get_quick_diagnostics(&self, url: &Url) -> Vec<Diagnostic> {
         let maybe_file = self.get_file(url);
 
         if let Some(file) = maybe_file {
@@ -103,7 +135,7 @@ impl Workspace {
         }
     }
 
-    pub fn get_full_diagnostics(&self, url: &Url) -> Vec<Diagnostic> {
+    fn get_full_diagnostics(&self, url: &Url) -> Vec<Diagnostic> {
         let maybe_file = self.get_file(url);
 
         if let Some(file) = maybe_file {
@@ -111,10 +143,5 @@ impl Workspace {
         } else {
             vec![]
         }
-    }
-
-    pub fn update_settings(&mut self, settings: Value) {
-        self.settings = Settings::parse(settings);
-        info!("Settings: {:?}", self.settings);
     }
 }

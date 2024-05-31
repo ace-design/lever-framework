6925e7b8ebba76964cb136a85f1f493e68f90bf5
Lachance, Alexandre <alexandrelachance@me.com>
2024-05-31 15:48:05 -0400

    Cleanup framework exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub use lever_core;
-pub use lever_gen;
+pub use lever_core::{Ast, HighlightType, Import, Node, NodeKind, Setup, Symbol, Translator};
+pub use lever_gen::rules_translator;
+pub use {indextree, tree_sitter};
 
-pub use indextree;
-pub use tree_sitter;
+pub use crate::indextree::NodeId;
 
 pub async fn start_server(setup: &lever_core::Setup) {
     lever_core::start_server(setup).await
